1777835d89f88c83df3687b9a6c21ce8fb942fc6
LaTisha Habersham <72284231+llhabers@users.noreply.github.com>
2021-01-20 18:13:22 -0500

    update

--- EmployeeSQL/employee_information.sql
+++ EmployeeSQL/employee_information.sql
@@ -1,15 +1,17 @@
--- -- SELECT *
--- -- FROM dept_emp;
+SELECT *
+FROM departments;
 
--- "\\copy public.dept_emp (emp_no, dept_no) FROM '/Users/lhabersham/code/sql-challenge/data/dept_emp.csv' DELIMITER ',' CSV HEADER QUOTE '\"' ESCAPE '''';""
+SELECT *
+FROM dept_emp;
 
--- COPY dept_emp [ ( emp_no, dept_no ) ]
---     FROM { dept_emp.csv | STDIN }
---     [ [ WITH ]
---           [ BINARY ]
---           [ DELIMITER AS ',' ]
---           [ NULL AS 'null_string' ]
---           [ CSV [ HEADER ]
---                 [ QUOTE AS \" ]
---                 [ ESCAPE AS ;" ]
---                 [ FORCE NOT NULL emp_no, dept_no ]]]
+SELECT *
+FROM dept_manager;
+
+SELECT *
+FROM employees;
+
+SELECT *
+FROM salaries;
+
+SELECT *
+FROM title;
