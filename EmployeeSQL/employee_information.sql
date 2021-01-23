e491b56bc5c4b0a1b3b289092a582581ba3d0c87
LaTisha Habersham <72284231+llhabers@users.noreply.github.com>
2021-01-23 14:41:48 -0500

    updated coding

--- EmployeeSQL/employee_information.sql
+++ EmployeeSQL/employee_information.sql
@@ -1,17 +1,67 @@
+CREATE TABLE "departments" (
+    "dept_no" serial NOT NULL,
+    "dept_name" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no", "dept_name"
+     )
+);
+
 SELECT *
 FROM departments;
 
-SELECT *
-FROM dept_emp;
+DROP TABLE departments;
 
-SELECT *
-FROM dept_manager;
+CREATE TABLE "dept_emp" (
+    "emp_no" id   NOT NULL,
+    "dept_no" serial   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no"
+     )
+);
 
-SELECT *
-FROM employees;
+CREATE TABLE "dept_manager" (
+    "emp_no" id   NOT NULL,
+    "dept_no" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
+);
 
-SELECT *
-FROM salaries;
+CREATE TABLE "emplyoees" (
+    "emp_no" id   NOT NULL,
+    "emp_title_id" serial   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "sex" VARCHAR   NOT NULL,
+    "hire_date" DATE   NOT NULL
+);
 
-SELECT *
-FROM title;
+CREATE TABLE "salaries" (
+    "emp_no" id   NOT NULL,
+    "salary" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "salary"
+     )
+);
+
+CREATE TABLE "title" (
+    "title" VARCHAR   NOT NULL,
+    "title_id" serial   NOT NULL,
+    CONSTRAINT "pk_title" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "emplyoees" ADD CONSTRAINT "fk_emplyoees_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "dept_emp" ("emp_no");
+
+ALTER TABLE "emplyoees" ADD CONSTRAINT "fk_emplyoees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "title" ("title_id");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "dept_manager" ("emp_no");
+
